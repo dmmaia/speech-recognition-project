bb96b3b522313766079dcb20ea7e0c2678db76f9
dmmaia <david_mmaia@hotmail.com>
2024-04-13 17:36:51 -0300

    feat: starting project

---
+++ app.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+import sounddevice as sd
+import wavio as wv
+
+filename = "./files/audio1.wav"
+freq = 44100
+duration = 10
+recording = sd.rec(int(duration * freq),
+                   samplerate=freq, channels=2)
+sd.wait()
+wv.write(filename, recording, freq, sampwidth=2)
+
+
+r = sr.Recognizer()
+
+
+with sr.AudioFile(filename) as source:
+    audio_data = r.record(source)
+    text = r.recognize_sphinx(audio_data)
+    print(text)
